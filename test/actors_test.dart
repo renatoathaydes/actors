480d521ca3093779bf3642b8985704f69e191ce1
renatoathaydes <renatoathaydes@hotmail.com>
2019-02-26 22:29:03 +0100

    More tests.

--- test/actors_test.dart
+++ test/actors_test.dart
@@ -6,6 +6,27 @@ class IntParserActor with Handler<String, int> {
   int handle(String message) => int.parse(message);
 }
 
+class DynamicActor with Handler {
+  @override
+  handle(message) {
+    switch (message.runtimeType) {
+      case String:
+        return 'string';
+      case int:
+        return 'integer';
+      default:
+        return -1;
+    }
+  }
+}
+
+class CounterActor with Handler<void, int> {
+  int count = 0;
+
+  @override
+  int handle(void message) => count++;
+}
+
 void main() {
   group('Typed Actor can run in isolate', () {
     Actor<String, int> actor;
@@ -19,6 +40,34 @@ void main() {
     });
     test('error is propagated to caller', () {
       expect(actor.send('x'), throwsFormatException);
+    });
+  });
+
+  group('Untyped Actor can run in isolate', () {
+    Actor actor;
+
+    setUp(() {
+      actor = Actor(DynamicActor());
+    });
+
+    test('can handle messages async', () async {
+      expect(await actor.send(10), equals('integer'));
+      expect(await actor.send('text'), equals('string'));
+      expect(await actor.send(true), equals(-1));
+    });
+  });
+
+  group('Actor can maintain internal state', () {
+    Actor<void, int> actor;
+
+    setUp(() {
+      actor = Actor(CounterActor()..count = 4);
+    });
+
+    test('actor uses internal state to respond', () async {
+      expect(await actor.send(null), equals(4));
+      expect(await actor.send(null), equals(5));
+      expect(await actor.send(null), equals(6));
     });
   });
 }
