673ac15c38b9d790426d4d8c0f110bd042cd1152
renatoathaydes <renatoathaydes@hotmail.com>
2019-08-09 22:06:58 +0200

    Non-implemented Web Worker Actor.

--- lib/src/web_worker/web_worker_actor.dart
+++ lib/src/web_worker/web_worker_actor.dart
@@ -1,5 +1,9 @@
 class ActorImpl {
-  void spawn() {}
+  void spawn(void Function(dynamic) entryPoint, message) {
+    throw 'Web Actors are not implemented yet!';
+  }
 
-  Future<void> close() async {}
+  Future<void> close() async {
+    throw 'Web Actors are not implemented yet!';
+  }
 }
