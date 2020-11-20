9ee6fe8fb2cde0c7c86cf957edf61c40725896bb
Renato Athaydes <renato@athaydes.com>
2020-11-20 22:06:02 +0100

    Better handling of null is example.

--- example/basic_example.dart
+++ example/basic_example.dart
@@ -10,6 +10,6 @@ String sayHi(String name) => withIso('Hi ${name}!');
 void main() async {
   print(withIso('main'));
   final actor = Actor.of(sayHi);
-  print(await actor.send(Platform.environment['USER']!));
+  print(await actor.send(Platform.environment['USER'] ?? 'anonymous'));
   await actor.close();
 }
